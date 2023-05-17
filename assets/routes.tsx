dc55bdd166e89c1613450138047167fa24976d36
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-18 00:08:28 +0300

    Update routes.tsx

--- assets/routes.tsx
+++ assets/routes.tsx
@@ -1,95 +1,51 @@
 import PrivateRoute from "components/PrivateRoute";
 
 import Home from "pages/Home";
-
 import HomeLayout from "views/layouts/HomeLayout";
 
 export type Route = IndexRoute | PathRoute;
 
 interface IndexRoute {
-
     index: true;
-
     name: string;
-
     element: JSX.Element;
-
     children?: Route[];
-
     auth?: true;
-
 }
 
 interface PathRoute {
-
     path: string;
-
     name: string;
-
     element: JSX.Element;
-
     children?: Route[];
-
     auth?: true;
-
 }
 
 const routes: Route[] = [
-
     {
-
         name: 'home',
-
         path: '/',
-
         element: <HomeLayout />,
-
         children: [
-
             {
-
                 name: 'index',
-
                 index: true,
-
                 element: <Home />
-
             },
-
-            {
-
-                name: 'secret',
-
-                path: 'secret',
-
-                auth: true,
-
-                element: <div>Secret! <Home /></div>
-
-            }
-
         ]
-
     }
-
 ];
 
 const RouteMap = (routeList: Route[]) => routeList.map(route => {
-
     if (route?.auth) {
-
         route.element = <PrivateRoute>{route.element}</PrivateRoute>
-
     }
 
     if (route?.children) {
-
         route.children = RouteMap(route.children);
-
     }
 
     return route;
-
 });
 
 export default RouteMap(routes);
