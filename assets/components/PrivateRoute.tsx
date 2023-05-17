0f6f68adaf4673ab006546d4db02cb107d535ed3
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-18 00:41:05 +0300

    Update PrivateRoute.tsx

--- assets/components/PrivateRoute.tsx
+++ assets/components/PrivateRoute.tsx
@@ -1,12 +1,14 @@
 import { useMain } from "context/Main";
 import { Navigate } from "react-router-dom";
 import url from "utils/url";
+import toast from "react-hot-toast";
 
 export default function PrivateRoute({ children }: { children: JSX.Element }) {
     /*
     const { user } = useMain();
 
     if (!user) {
+        toast.error("403");
         return <Navigate to={url('home')} />
     }
     */
