9810f07f51ad55d3e76842f55404c21b31871781
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-18 00:10:02 +0300

    Update PrivateRoute.tsx

--- assets/components/PrivateRoute.tsx
+++ assets/components/PrivateRoute.tsx
@@ -1,25 +1,15 @@
 import { useMain } from "context/Main";
-
 import { Navigate } from "react-router-dom";
-
 import url from "utils/url";
 
 export default function PrivateRoute({ children }: { children: JSX.Element }) {
-
     /*
-
     const { user } = useMain();
 
     if (!user) {
-
         return <Navigate to={url('home')} />
-
     }
-
     */
 
-   
-
     return children;
-
 }
