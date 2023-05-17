ef6f79d1b6b79f5cebc65af031dbfd714b8eab87
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-18 00:39:44 +0300

    Update App.tsx

--- assets/App.tsx
+++ assets/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-
 import { useRoutes } from 'react-router-dom';
-
 import { Toaster } from 'react-hot-toast';
-
 import routes from 'routes';
 
 function App() {
-
   return (
-
     <>
-
       <Toaster position='top-right' />
-
       {useRoutes(routes as any)}
-
     </>
-
   );
-
 }
 
 export default App;
