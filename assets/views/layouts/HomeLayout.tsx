29e9c487397f8f9fc29d9b58615f254bfc45ae20
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-17 23:52:01 +0300

    Create HomeLayout.tsx

---
+++ assets/views/layouts/HomeLayout.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Outlet } from 'react-router-dom'
+
+import Footer from 'views/Footer'
+
+import Header from 'views/Header'
+
+function HomeLayout() {
+
+    return (
+
+        <div>
+
+            <Header />
+
+            <main>
+
+                <Outlet />
+
+            </main>
+
+            <Footer />
+
+        </div>
+
+    )
+
+}
+
+export default HomeLayout
