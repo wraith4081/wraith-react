1d5cfa2354ccd1523adab19d089119cf2775cb69
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-17 23:48:16 +0300

    Create App.tsx

---
+++ assets/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { useRoutes } from 'react-router-dom';
+
+import { Toaster } from 'react-hot-toast';
+
+import routes from 'routes';
+
+function App() {
+
+  return (
+
+    <>
+
+      <Toaster position='top-right' />
+
+      {useRoutes(routes as any)}
+
+    </>
+
+  );
+
+}
+
+export default App;
