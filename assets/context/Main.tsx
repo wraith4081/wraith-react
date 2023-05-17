08254f18a8e2e61265aa57042d6209d93ffaf2b2
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-18 00:10:33 +0300

    Update Main.tsx

--- assets/context/Main.tsx
+++ assets/context/Main.tsx
@@ -3,25 +3,17 @@ import React, { createContext, useContext, useState } from "react";
 const Context = createContext(undefined as any);
 
 export const MainProvider = ({children}: {children: JSX.Element}) => {
-
     const [user, setUser] = useState(false);
 
     const data = {
-
         user,
-
         setUser
-
     };
 
     return <Context.Provider value={data}>{children}</Context.Provider>
-
 }
 
 export const useMain = () => useContext(Context) as {
-
     user: any;
-
     setUser: React.Dispatch<React.SetStateAction<any>>;
-
 }
