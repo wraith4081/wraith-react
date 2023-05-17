3dd1603d64b9589d3a9adab5c3cee6d476d739dd
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-05-18 00:12:22 +0300

    Update HomeLayout.tsx

--- assets/views/layouts/HomeLayout.tsx
+++ assets/views/layouts/HomeLayout.tsx
@@ -1,31 +1,18 @@
 import React from 'react'
-
 import { Outlet } from 'react-router-dom'
-
 import Footer from 'views/Footer'
-
 import Header from 'views/Header'
 
 function HomeLayout() {
-
     return (
-
         <div>
-
             <Header />
-
             <main>
-
                 <Outlet />
-
             </main>
-
             <Footer />
-
         </div>
-
     )
-
 }
 
 export default HomeLayout
